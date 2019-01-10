5f9ea6094a03f65618d0d6fc9e5247a191d2560e
zc201198 <1397310999@qq.com>
2019-01-10 15:36:46 +0800

    Site updated: 2019-01-10 15:36:41

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,15 +1,15 @@
-<!--�������Ц����-->
+<!--浏览器搞笑标题-->
  var OriginTitle = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = '�c(��-`����-)���������ߣ�';
+         document.title = 'ヽ(●-`Д′-)ノ你丑你就走！';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = '�d(???3)����˧�ͻ�����' + OriginTitle;
+         document.title = 'ヾ(???3)ノ你帅就回来！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
